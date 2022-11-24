1b10daba5cdabedcf007c02cd9cea9881959ee7c
abdul momin <mominurrahman10748@gmail.com>
2022-11-24 19:04:19 +0600

    category collection added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
@@ -12,12 +13,28 @@ app.get('/', (req, res) => {
 })
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tmuuwhy.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try{
+        const productsCategoriCollection = client.db("oldBooksHere1").collection("productsCategories1");
 
+        app.get('/productsCategoris', async(req, res) => {
+            const result = await productsCategoriCollection.find({}).toArray();
+            res.send(result);
+            console.log(result);
+        })
+    }
+    finally{
 
+    }
+}
+run().catch(err => console.log(err))
 
 
 app.listen(port, () => {
-console.log(`old books here server running on port ${port}`);
+    console.log(`old books here server running on port ${port}`);
 });
