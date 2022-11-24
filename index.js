548b3011ab018f46277aa33ae226546cd3df2b13
abdul momin <mominurrahman10748@gmail.com>
2022-11-24 13:25:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('welcome to old books here')
+})
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+console.log(`old books here server running on port ${port}`);
+});
